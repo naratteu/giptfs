69e2d883df77fc755298c50e2dda92866c30e96d
naratteu <skfhddlg@naver.com>
2026-01-01 19:07:43 +0900

    git clone --branch 처리에 실패할경우 수동 clone 구성

--- Clone.cs
+++ Clone.cs
@@ -30,30 +30,58 @@ partial class Program
                 branch = branch.TrimEnd('/');
                 var sparse = string.Concat(sgmts.Skip(5));
                 Cat(new { repo, branch, sparse });
-                await Run(g.WithArguments(["clone", "--branch", branch, "--sparse", url, dir]));
-                await Run(g.WithArguments(["sparse-checkout", "set", sparse]).WithWorkingDirectory(dir));
-                return dir;
+                try
+                {
+                    await Run(g.WithArguments(["clone", "--branch", branch, "--sparse", url, dir]));
+                    await Run(g.WithArguments(["sparse-checkout", "set", sparse]).WithWorkingDirectory(dir));
+                    //todo:아래로 통용되면 굳이 clone을 이용할 필요가 없나?
+                }
+                catch when (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                    var gd = g.WithWorkingDirectory(dir);
+                    await Run(gd.WithArguments(["init"]));
+                    await Run(gd.WithArguments(["sparse-checkout", "init", "--cone"]));
+                    await Run(gd.WithArguments(["sparse-checkout", "set", sparse]));
+                    await Run(gd.WithArguments(["remote", "add", "origin", url]));
+                    await Run(gd.WithArguments(["fetch", "origin", branch]));
+                    await Run(gd.WithArguments(["checkout", branch])); // 아마 브랜치가 아니라 태그나 커밋해시
+                }
+                break;
             case { Authority: GH or WGH, Segments: ["/", _, var repo, "tree/" or "blob/", var branch] sgmts } uri:
                 url = new Uri(uri, string.Concat(sgmts.Take(3))).OriginalString; // Fragment 도 제거됨.
                 dir = destDir ?? repo;
                 branch = branch.TrimEnd('/');
                 Cat(new { repo, branch });
-                await Run(g.WithArguments(["clone", "--branch", branch, url, dir]));
-                return dir;
+                try
+                {
+                    await Run(g.WithArguments(["clone", "--branch", branch, url, dir]));
+                    //todo:아래로 통용되면 굳이 clone을 이용할 필요가 없나?
+                }
+                catch when (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                    var gd = g.WithWorkingDirectory(dir);
+                    await Run(gd.WithArguments(["init"]));
+                    await Run(gd.WithArguments(["remote", "add", "origin", url]));
+                    await Run(gd.WithArguments(["fetch", "origin", branch]));
+                    await Run(gd.WithArguments(["checkout", branch])); // 아마 브랜치가 아니라 태그나 커밋해시
+                }
+                break;
             case { Authority: GGH, Segments: ["/", _, var repo, var revision] } uri:
                 throw new NotImplementedException("gist의 revision 을 어떻게 처리할지 아직 안만들엇어요");
-            case { Authority: GH or WGH or GGH, Segments: ["/", _, var repo] } uri: return await Default(uri, repo);
-            case { Segments: [.., var repo] } uri: return await Default(uri, repo);
-            async Task<string> Default(Uri uri, string repo)
+            case { Authority: GH or WGH or GGH, Segments: ["/", _, var repo] } uri: await Default(uri, repo); break;
+            case { Segments: [.., var repo] } uri: await Default(uri, repo); break;
+            Task Default(Uri uri, string repo)
             {
                 url = uri.OriginalString[..^uri.Fragment.Length]; // Fragment 제거.
                 dir = destDir ?? repo;
                 Cat(new { repo });
-                await Run(g.WithArguments(["clone", url, dir]));
-                return dir;
+                return Run(g.WithArguments(["clone", url, dir]));
             }
             default:
                 throw new NotImplementedException("clone ignored : 깃허브나 유효한 url이 아니네요.");
         }
+        return dir;
     }
 }
