d77c2e8a544a55b6b7658288daa0225f6192c9b9
naratteu <skfhddlg@naver.com>
2026-01-01 18:00:56 +0900

    git clone 형태를 따라서 파라미터명 변경, 디렉토리명 입력

--- Clone.cs
+++ Clone.cs
@@ -2,43 +2,44 @@
 partial class Program
 {
     /// <summary>clone 후 즉시 vscode로 열어봅니다.</summary>
-    /// <param name="sourceUrl">github 저장소 url. 브랜치, 하위경로를 포함할 수 있습니다.</param>
-    public async Task Code([Argument] string sourceUrl)
+    /// <param name="repo">github 저장소 url. 브랜치, 하위경로 등을 포함할 수 있습니다.</param>
+    public async Task Code([Argument] string repo, [Argument] string? dir = null)
     {
-        var repo = await Clone_(sourceUrl);
+        repo = await Clone_(repo, dir);
         await code.WithArguments([repo]).ExecuteAsync();
     }
 
     /// <summary>github를 위한 좀더 간편한 clone.</summary>
-    /// <param name="sourceUrl">github 저장소 url. 브랜치, 하위경로를 포함할 수 있습니다.</param>
-    public async Task Clone([Argument] string sourceUrl)
-         => await Clone_(sourceUrl);
-    async Task<string> Clone_(string sourceUrl)
+    /// <param name="repo">github 저장소 url. 브랜치, 하위경로 등을 포함할 수 있습니다.</param>
+    public Task Clone([Argument] string repo, [Argument] string? dir = null) => Clone_(repo, dir);
+    async Task<string> Clone_(string sourceUrl, string? destDir)
     {
         var g = git | (Console.WriteLine, Console.Error.WriteLine);
-        static void Cat(string url, object o) { Console.WriteLine(url); Console.WriteLine(o); }
+        string url, dir;
+        void Cat(object o) { Console.WriteLine(new { url, dir }); Console.WriteLine(o); }
         static Task Run(Command c) { Console.WriteLine(c); return c.ExecuteAsync(); }
         const string
             GH = "github.com",
             WGH = "www.github.com",
-            GGH = "gist.github.com"
-        string url;
+            GGH = "gist.github.com";
         switch (new Uri(sourceUrl))
         {
             case { Authority: GH or WGH, Segments: ["/", _, var repo, "tree/" or "blob/", var branch, _, ..] sgmts } uri:
                 url = new Uri(uri, string.Concat(sgmts.Take(3))).OriginalString; // Fragment 도 제거됨.
+                dir = destDir ?? repo;
                 branch = branch.TrimEnd('/');
                 var sparse = string.Concat(sgmts.Skip(5));
-                Cat(url, new { repo, branch, sparse });
-                await Run(g.WithArguments(["clone", "--branch", branch, "--sparse", url, repo]));
-                await Run(g.WithArguments(["sparse-checkout", "set", sparse]).WithWorkingDirectory(repo));
-                return repo;
+                Cat(new { repo, branch, sparse });
+                await Run(g.WithArguments(["clone", "--branch", branch, "--sparse", url, dir]));
+                await Run(g.WithArguments(["sparse-checkout", "set", sparse]).WithWorkingDirectory(dir));
+                return dir;
             case { Authority: GH or WGH, Segments: ["/", _, var repo, "tree/" or "blob/", var branch] sgmts } uri:
                 url = new Uri(uri, string.Concat(sgmts.Take(3))).OriginalString; // Fragment 도 제거됨.
+                dir = destDir ?? repo;
                 branch = branch.TrimEnd('/');
-                Cat(url, new { repo, branch });
-                await Run(g.WithArguments(["clone", "--branch", branch, url, repo]));
-                return repo;
+                Cat(new { repo, branch });
+                await Run(g.WithArguments(["clone", "--branch", branch, url, dir]));
+                return dir;
             case { Authority: GGH, Segments: ["/", _, var repo, var revision] } uri:
                 throw new NotImplementedException("gist의 revision 을 어떻게 처리할지 아직 안만들엇어요");
             case { Authority: GH or WGH or GGH, Segments: ["/", _, var repo] } uri: return await Default(uri, repo);
@@ -46,9 +47,10 @@ partial class Program
             async Task<string> Default(Uri uri, string repo)
             {
                 url = uri.OriginalString[..^uri.Fragment.Length]; // Fragment 제거.
-                Cat(url, new { repo });
-                await Run(g.WithArguments(["clone", url, repo]));
-                return repo;
+                dir = destDir ?? repo;
+                Cat(new { repo });
+                await Run(g.WithArguments(["clone", url, dir]));
+                return dir;
             }
             default:
                 throw new NotImplementedException("clone ignored : 깃허브나 유효한 url이 아니네요.");
